3ceed158b8e77b9ee0d95417c98a676cc0611709
pratik jambhulkar <pratikj009@gmail.com>
2020-08-28 21:50:16 +0530

    Add remaining models and manager

--- invoice/models.py
+++ invoice/models.py
@@ -4,11 +4,25 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
+from invoice.managers import DefaultManager
+from invoice.utils import generate_invoice_number
 
-class User(AbstractBaseUser, PermissionsMixin):
+
+class CommonField(models.Model):
+    # Fields
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = DefaultManager()
+
+    class Meta:
+        abstract = True
+
+
+class User(AbstractBaseUser, PermissionsMixin, CommonField):
     # Fields
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    email = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True)
     name = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
 
@@ -36,3 +50,49 @@ class User(AbstractBaseUser, PermissionsMixin):
     # Meta
     class Meta:
         db_table = 'users'
+
+
+class Company(CommonField):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, unique=True)
+    address = models.TextField()
+    email = models.EmailField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'companies'
+
+
+class Invoice(CommonField):
+    invoice_number = models.CharField(max_length=8, primary_key=True, editable=False, default=generate_invoice_number)
+    terms = models.TextField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deu_date = models.DateTimeField()
+
+    digitized = models.BooleanField(default=False)
+    digitized_by = models.ForeignKey('User', on_delete=models.CASCADE, null=True)
+    purchaser = models.ForeignKey('Company', on_delete=models.CASCADE, null=True, related_name='purchaser')
+    vendor = models.ForeignKey('Company', on_delete=models.CASCADE, null=True, related_name='vendor')
+
+    class Meta:
+        db_table = 'invoices'
+
+
+class InvoiceItem(CommonField):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    quantity = models.IntegerField(default=1)
+    price = models.FloatField()
+    amount = models.FloatField()
+
+    invoice = models.ForeignKey('Invoice', on_delete=models.CASCADE, null=True, related_name='invoice_items')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'invoice_items'
